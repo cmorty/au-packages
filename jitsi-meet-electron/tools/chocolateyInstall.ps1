4b6800d27825db099810bb9f55cc876d16ecb3a1
Moritz 'Morty' Strübe <moritz.struebe@redheads.de>
2020-05-14 23:52:38 +0200

    Install globally and provide application with package

--- jitsi-meet-electron/tools/chocolateyInstall.ps1
+++ jitsi-meet-electron/tools/chocolateyInstall.ps1
@@ -1,16 +1,36 @@
 ﻿$ErrorActionPreference = 'Stop'
 
+
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
 $packageArgs = @{
-  packageName    = 'jitsi-meet-electron'
-  fileType       = 'exe'
-  softwareName   = 'Jitsi Meet Electron'
-
-  checksum       = '52840d84646b4ec498025e647ce178a64bbbb3cf4ba1656569c2f118cf8f3ca4'
+  packageName    = "jitsi-meet-electron"
+  file    = "$toolsdir\app-32.7z"
+  file64  = "$toolsdir\app-64.7z"
+  unzipLocation  = "$toolsDir"
+  checksum       = '0363AF8E59CDA3B7428AD147FE32CB01122463EB7B866808DD8646212CA6F712'
+  checksum64     = '9902903E8ACCABA33AC7AC7F774C1381A030C0B5A89CDF2A76BCCEFE6606AE07'
   checksumType   = 'sha256'
-  url            = 'https://github.com/jitsi/jitsi-meet-electron/releases/download/v2.0.2/jitsi-meet.exe'
-
-  silentArgs     = '/S /allusers'
-  validExitCodes = @(0)
+  ChecksumType64 = 'sha256'
 }
 
-Install-ChocolateyPackage @packageArgs
+
+Write-Verbose "Unpacking"
+Install-ChocolateyZipPackage @packageArgs
+
+Get-ChildItem $toolsdir\*.7z | ForEach-Object { Remove-Item $_ -ea 0; if (Test-Path $_) { Set-Content "$_.ignore" } }
+
+Write-Verbose "Searching for exe"
+$files = get-childitem "$toolsdir" -include "*.exe" -recurse
+
+
+foreach ($file in $files) {
+  #generate an ignore file
+  if($file.Name -like "Jitsi*") {
+    Write-Verbose "Crating Start Menu enty for $($file.BaseName)"
+    Install-ChocolateyShortcut -shortcutFilePath "$($env:ProgramData)\Microsoft\Windows\Start Menu\Programs\$($file.BaseName).lnk" -targetPath "$file"
+  } else {
+    Write-Verbose "Ignoring $($file.Name)"
+    New-Item "$file.ignore" -type file -force | Out-Null
+  }
+}
--- jitsi-meet-electron/update.ps1
+++ jitsi-meet-electron/update.ps1
@@ -7,17 +7,58 @@ $releases = "$domain/$ghproj/releases/latest"
 function global:au_SearchReplace {
   @{
     ".\tools\chocolateyInstall.ps1" = @{
-      "(?i)(^\s*url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
       "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
       "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+      "(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+      "(?i)(^\s*checksumType64\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType64)'"
     }
     ".\jitsi-meet-electron.nuspec" = @{
       "\<releaseNotes\>.+" = "<releaseNotes>$($Latest.ReleaseNotes)</releaseNotes>"
     }
+    ".\tools\VERIFICATION.txt" = @{
+      "(?i)(URL:).*"             = "`${1} $($Latest.URL32)"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
+      "(?i)(checksum64:).*"      = "`${1} $($Latest.Checksum64)"
+    }
   }
 }
 
+
+function global:au_BeforeUpdate {
+  set-alias 7z $Env:chocolateyInstall\tools\7z.exe
+
+  $toolsp="$PSScriptRoot\tools"
+  $downtemp="$PSScriptRoot\temp"
+  Remove-Item "$downtemp" -Recurse -Force -ea ignore
+  mkdir "$downtemp"
+  $downf = "$downtemp\jtisi.exe"
+  $ProgressPreference = 'SilentlyContinue'
+  Invoke-WebRequest $Latest.URL32 -OutFile  $downf
+  7z x $downf "-o$downtemp"
+
+  Remove-Item "$toolsp\*.7z"
+  Move-Item "$downtemp\`$PLUGINSDIR\*.7z" $toolsp
+  Remove-Item "$downtemp" -Recurse -Force -ea ignore
+
+  #Based on Get-RemoteFiles
+
+  $f32="app-32.7z"
+  $f64="app-64.7z"
+  $Algorithm = 'sha256'
+
+  $global:Latest.Checksum32 = Get-FileHash "$toolsp\$f32" -Algorithm $Algorithm | % Hash
+  $global:Latest.ChecksumType32 = $Algorithm
+  $global:Latest.FileName32 = $f32
+
+  $global:Latest.Checksum64 = Get-FileHash "$toolsp\$f64" -Algorithm $Algorithm | % Hash
+  $global:Latest.ChecksumType64 = $Algorithm
+  $global:Latest.FileName64 = $f32
+}
+
+
 function global:au_GetLatest {
+  $ProgressPreference = 'SilentlyContinue'
   $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
 
   $re    = '\.exe$'
@@ -34,4 +75,9 @@ function global:au_GetLatest {
   }
 }
 
-update -ChecksumFor 32
+try {
+  update -ChecksumFor none
+} catch {
+  $ignore = 'Unable to connect to the remote server'
+  if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
+}
