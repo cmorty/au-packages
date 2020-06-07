35cf04120d1dadaae73609508092d0ad6534aee2
Moritz 'Morty' Strübe <moritz.struebe@redheads.de>
2020-06-07 22:02:08 +0200

    Add pingtracer

---
+++ pingtracer/tools/chocolateyInstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop'
+
+
+$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+
+$packageArgs = @{
+  unzipLocation  = "$toolsDir"
+  packageName    = "pingtracer"
+  file           = 'tools/PingTracer.1.8_x32.zip'
+  checksum       = '292777976917364EA471BFED8CCDEF105761521DD90BDF0092D043B3856BFE04'
+  checksumType   = 'sha256'
+}
+
+
+Write-Verbose "Unpacking"
+Install-ChocolateyZipPackage @packageArgs
+
+Write-Verbose "Searching for exe"
+$files = get-childitem "$toolsdir" -include "*.exe" -recurse
+
+
+# This will create a Shim for all exe and will add a line chocoUninstall.ps1 to remove it on uninstall
+foreach ($file in $files) {
+  Write-Verbose "Crating Start Menu enty for $($file.BaseName)"
+  $newlink ="$($env:ProgramData)\Microsoft\Windows\Start Menu\Programs\$($file.BaseName).lnk"
+  Install-ChocolateyShortcut -shortcutFilePath $newlink  -targetPath "$file"
+  Add-Content "$toolsdir\chocoUninstall.ps1" -Value "Remove-Item `"$($newlink)`" -Force -ea ignore"
+}
---
+++ pingtracer/update.ps1
@@ -0,0 +1,47 @@
+﻿import-module au
+
+$domain   = 'https://github.com'
+$ghproj = 'bp2008/pingtracer'
+$releases = "$domain/$ghproj/releases/latest"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*file\s*=\s*)('.*')" = "`$1'tools/$($Latest.Filename32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+    ".\pingtracer.nuspec" = @{
+      "\<releaseNotes\>.+" = "<releaseNotes>$($Latest.ReleaseNotes)</releaseNotes>"
+    }
+    ".\tools\VERIFICATION.txt" = @{
+      "(?i)(URL:).*"             = "`${1} $($Latest.URL32)"
+      "(?i)(checksum type:).*"   = "`${1} $($Latest.ChecksumType32)"
+      "(?i)(checksum32:).*"      = "`${1} $($Latest.Checksum32)"
+    }
+  }
+}
+
+
+function global:au_BeforeUpdate { Get-RemoteFiles -Purge }
+
+function global:au_GetLatest {
+  #$ProgressPreference = 'SilentlyContinue'
+  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+
+  $re    = '\.zip$'
+  $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+  $version  = ($url -split '/' | Select-Object -Last 1 -Skip 1 )
+
+  $releaseNotesUrl = $download_page.BaseResponse.ResponseUri.AbsoluteUri
+
+  @{
+    URL32 = $domain + $url
+    Version = $version
+    ReleaseNotes = $releaseNotesUrl
+  }
+}
+
+
+update -ChecksumFor none
